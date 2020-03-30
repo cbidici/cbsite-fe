e1f5daaf9b185e3b836b371001fbdd0e04cce627
cbidici <coskun.bidici@udemy.com>
2020-03-30 23:16:58 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Main from  './main/Main';
+import { BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <Main/>
+    <Router>
+      <Main/>
+    </Router>
   );
 }
 
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Layout} from 'antd';
 import Blog from '../blog/Blog'
+import { Link, Route } from 'react-router-dom'
 const {Header, Content, Footer, Sider} = Layout
 
 class Main extends React.Component {
     render() {
         return(
             <Layout>
-                <Header>Header</Header>
+                <Header>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/blog/'}>Blog</Link>
+                </Header>
                 <Content>
-                    <Blog/>
+                    <Route exact={true} path='/' render={()=>(<div>Home</div>)} />
+                    <Route path='/blog/' component={Blog} />
                 </Content>
                 <Footer>Footer</Footer>
             </Layout>
