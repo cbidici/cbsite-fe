aee03fa450f881b8e1ac467668c1a837dd11175a
cbidici <coskun.bidici@udemy.com>
2020-04-03 22:56:59 +0300

    Add backend query

--- src/blog/PostCard.js
+++ src/blog/PostCard.js
@@ -8,9 +8,9 @@ class Post extends React.Component {
         return (
             <Card style={{"border":0}}>
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in felis placerat, tempus nunc blandit, laoreet libero. Nunc eget consequat turpis. Ut augue enim, faucibus vitae metus ac, pretium luctus dui. In mollis accumsan dui, sed tristique ex. Nunc vehicula, sapien quis porttitor porttitor, tellus lorem convallis est, eu condimentum purus sem a lacus. Vestibulum metus tellus, sagittis id varius pellentesque, efficitur vitae nibh. Sed blandit fermentum nulla eu pellentesque. Suspendisse tristique venenatis ipsum, sed aliquam justo mattis in. Vestibulum venenatis lorem turpis, quis vehicula lectus porttitor a. In porta dictum varius. Fusce justo turpis, tempor sit amet mauris facilisis, fermentum convallis mauris.</Card.Text>
-                    <Link to="/blog/1/" className="card-link float-right">Read</Link>
+                    <Card.Title>{this.props.post.title}</Card.Title>
+                    <Card.Text>{this.props.post.text}</Card.Text>
+                    <Link to={"/blog/"+this.props.post.id+"/"} className="card-link float-right">Read</Link>
                 </Card.Body>
             </Card>
         );
--- src/blog/PostList.js
+++ src/blog/PostList.js
@@ -2,23 +2,31 @@ import React from 'react'
 import PostCard from './PostCard'
 
 class PostList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state= {
+            posts:[]
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://127.0.0.1:8000/api/blog/posts/')
+        .then(res=> res.json())
+        .then((data)=>{
+            this.setState({
+                posts:data
+            });
+        }).catch((e) => {console.log(e)})
+    }
+
     render() {
+        console.log(this.state)
         return(
             <div>
-                <PostCard/>
-                <br/>
-                <PostCard/>
-                <br/>
-                <PostCard/>
-                <br/>
-                <PostCard/>
-                <br/>
-                <PostCard/>
-                <br/>
-                <PostCard/>
-                <br/>
-                <PostCard/>
-                <br/>
+                {this.state.posts.map((post)=>(
+                    <PostCard key={post.id} post={post}/>
+                ))}
             </div>
         );
     }
