7072db56af43a76854e94deb770a55a3174808ad
cbidici <coskun.bidici@udemy.com>
2020-04-30 20:41:08 +0300

    Remove editormd add marked

--- src/blog/MarkdownHtml.js
+++ src/blog/MarkdownHtml.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import 'editor.md/css/editormd.preview.css';
-require('editor.md/lib/prettify.min.js')
-const editormd = require('editor.md/editormd.js')();
-window.marked = require('editor.md/lib/marked.min.js')
+import DOMPurify from 'dompurify';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import htmlbars from 'highlight.js/lib/languages/htmlbars';
+import css from 'highlight.js/lib/languages/css';
+import java from 'highlight.js/lib/languages/java';
+import python from 'highlight.js/lib/languages/python';
+import bash from 'highlight.js/lib/languages/bash';
+import diff from 'highlight.js/lib/languages/diff';
+import sql from 'highlight.js/lib/languages/sql';
+import xml from 'highlight.js/lib/languages/xml';
+import 'highlight.js/styles/github.css';
+const marked = require('marked');
 
 
 class MarkdownHtml extends React.Component {
     constructor(props) {
         super(props);
-        this.htmlview = null;
+        this.state = {
+            preview : "",
+            text : "",
+        }
+
+        hljs.registerLanguage('javascript', javascript);
+        hljs.registerLanguage('htmlbars', htmlbars);
+        hljs.registerLanguage('css', css);
+        hljs.registerLanguage('java', java);
+        hljs.registerLanguage('python', python);
+        hljs.registerLanguage('bash', bash);
+        hljs.registerLanguage('diff', diff);
+        hljs.registerLanguage('sql', sql);
+        hljs.registerLanguage('xml', xml);
     }
 
     loadEditor = () => {
-        var text = this.props.text;
-        this.htmlview = editormd.markdownToHTML("markdown-view", {
-            markdown : text,
-            
-        });
+        if(this.props.text && this.state.text !== this.props.text ) {
+            marked.setOptions({
+                gfm:true,
+                breaks:true,
+                highlight: function(code, lang, callback) {
+                    if(hljs.getLanguage(lang)) {
+                        return hljs.highlight(lang, code).value;
+                    }
+                    else {
+                        return code;
+                    }
+                }
+            });
+            var previewHtml = DOMPurify.sanitize(marked(this.props.text));
+            this.setState({
+                preview:previewHtml,
+                text:this.props.text,
+            });
+        }
     }
 
     componentDidMount() {
@@ -29,9 +65,7 @@ class MarkdownHtml extends React.Component {
 
     render() {
         return (
-            <div>
-                <div id="markdown-view"></div>
-            </div>
+            <div dangerouslySetInnerHTML={{__html: this.state.preview}}></div>
         );
 
     }
